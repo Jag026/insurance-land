447f3bdfefdea2ac037178b757cc9d1506f0a5f0
Drew <DGriffin@appacademy.io>
2022-10-03 21:46:57 -0500

    Initialized Express App

---
+++ backend/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+require('express-async-errors');
+const morgan = require('morgan');
+const cors = require('cors');
+const csurf = require('csurf');
+const helmet = require('helmet');
+const cookieParser = require('cookie-parser');
+
+const { environment } = rquire('./config');
+const isProduction = environment === 'production';
+
+const app = express();
+
+app.use(morgan('dev'));
