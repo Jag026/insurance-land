ab519e6d5ea20221ec81aa3d3ef29946b3d59738
Drew <DGriffin@appacademy.io>
2022-10-04 08:40:54 -0500

    Add an API router1

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -5,3 +5,13 @@ router.post('/test', function (req, res) {
 })
 
 module.exports = router;
+
+
+fetch('/api/test', {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "XSRF-TOKEN": `1kz6ujfT-X0HBm5Oyc4uJQNyicMRua66-buA`
+    },
+    body: JSON.stringify({ hello: 'world' })
+}).then(res = res.json()).then(data => console.log(data));
