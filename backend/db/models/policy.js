ce91c4caa7a85194b7e2a3c7b6024900c2253c24
Drew <DGriffin@appacademy.io>
2022-10-05 22:10:03 -0500

    Add policy methods

--- backend/db/models/policy.js
+++ backend/db/models/policy.js
@@ -3,6 +3,22 @@ const { Model, Validator } = require('sequelize');
 
 module.exports = (sequelize, DataTypes) => {
   class Policy extends Model {
+
+   toSafeObject() {
+      const { id, name, premium, description, companyName } = this; // context will be the User instance
+      return { id, name, premium, description, companyName };
+   }
+
+    static async addPolicy({ name, premium, description, companyName}) {
+      const policy = await Policy.create({
+        name,
+        premium,
+        description,
+        companyName
+      });
+      return await Policy.findByPk(policy.id);
+    }
+
     static associate(models) {
       // define association here
     }
