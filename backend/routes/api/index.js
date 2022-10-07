002a27823f7009621442822b7256baae0cc6dff5
Drew <DGriffin@appacademy.io>
2022-10-06 23:09:54 -0500

    Added auth for Company table

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,16 +1,28 @@
 const router = require('express').Router();
 const { restoreUser } = require('../../utils/auth.js');
+const { restoreCompany } = require('../../utils/auth.js');
 
 // GET /api/set-token-cookie
 const { setTokenCookie } = require('../../utils/auth.js');
+const { setTokenCookieCompany } = require('../../utils/auth.js');
+
 const { User } = require('../../db/models');
+const { Company } = require('../../db/models');
 
 router.use(restoreUser);
+router.use(restoreCompany);
 
 router.get(
   '/restore-user',
   (req, res) => {
     return res.json(req.user);
+  }
+);
+
+router.get(
+  '/restore-company',
+  (req, res) => {
+    return res.json(req.company);
   }
 );
 
@@ -24,6 +36,16 @@ router.get('/set-token-cookie', async (_req, res) => {
   return res.json({ user });
 });
 
+router.get('/set-token-cookie-company', async (_req, res) => {
+  const company = await Company.findOne({
+      where: {
+        username: 'fakecompany55'
+      }
+    });
+  setTokenCookieCompany(res, company);
+  return res.json({ company });
+});
+
 // GET /api/require-auth
 const { requireAuth } = require('../../utils/auth.js');
 router.get(
@@ -33,5 +55,7 @@ router.get(
     return res.json(req.user);
   }
 );
+
+
 
 module.exports = router;
--- backend/utils/auth.js
+++ backend/utils/auth.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken');
 const { jwtConfig } = require('../config');
 const { User } = require('../db/models');
+const { Company } = require('../db/models');
 
 const { secret, expiresIn } = jwtConfig;
 
@@ -26,30 +27,76 @@ const setTokenCookie = (res, user) => {
 
   return token;
 };
+  // Sends a JWT Cookie
+  const setTokenCookieCompany = (res, company) => {
+    // Create the token.
+    const tokenCompany = jwt.sign(
+      { data: company.toSafeObject() },
+      secret,
+      { expiresIn: parseInt(expiresIn) } // 604,800 seconds = 1 week
+    );
 
-const restoreUser = (req, res, next) => {
-  // token parsed from cookies
-  const { token } = req.cookies;
-  req.user = null;
 
-  return jwt.verify(token, secret, null, async (err, jwtPayload) => {
-    if (err) {
+    const isProduction = process.env.NODE_ENV === "production";
+
+    // Set the token cookie
+    res.cookie('company-token', companyToken, {
+      maxAge: expiresIn * 1000, // maxAge in milliseconds
+      httpOnly: true,
+      secure: isProduction,
+      sameSite: isProduction && "Lax"
+    });
+
+    return companyToken;
+  };
+
+  const restoreUser = (req, res, next) => {
+    // token parsed from cookies
+    const { token } = req.cookies;
+    req.user = null;
+
+    return jwt.verify(token, secret, null, async (err, jwtPayload) => {
+      if (err) {
+        return next();
+      }
+
+      try {
+        const { id } = jwtPayload.data;
+        req.user = await User.scope('currentUser').findByPk(id);
+      } catch (e) {
+        res.clearCookie('token');
+        return next();
+      }
+
+      if (!req.user) res.clearCookie('token');
+
       return next();
-    }
+    });
+  };
+  
+  const restoreCompany = (req, res, next) => {
+    // token parsed from cookies
+    const { companyToken } = req.cookies;
+    req.company = null;
 
-    try {
-      const { id } = jwtPayload.data;
-      req.user = await User.scope('currentUser').findByPk(id);
-    } catch (e) {
-      res.clearCookie('token');
+    return jwt.verify(companyToken, secret, null, async (err, jwtPayload) => {
+      if (err) {
+        return next();
+      }
+
+      try {
+        const { id } = jwtPayload.data;
+        req.company = await Company.scope('currentCompany').findByPk(id);
+      } catch (e) {
+        res.clearCookie('token');
+        return next();
+      }
+
+      if (!req.company) res.clearCookie('token');
+
       return next();
-    }
-
-    if (!req.user) res.clearCookie('token');
-
-    return next();
-  });
-};
+    });
+  };
 
 // If there is no current user, return an error
 const requireAuth = function (req, _res, next) {
@@ -62,4 +109,4 @@ const requireAuth = function (req, _res, next) {
   return next(err);
 }
 
-module.exports = { setTokenCookie, restoreUser, requireAuth };
+module.exports = { setTokenCookie, restoreUser, requireAuth, restoreCompany, setTokenCookieCompany };
