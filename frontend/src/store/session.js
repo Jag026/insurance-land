ebcf2e649f027ddb304f3c3836b3b2ec26c04136
Drew <DGriffin@appacademy.io>
2022-10-14 22:19:35 -0500

    Add user signup component

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -55,4 +55,19 @@ export const restoreUser = () => async dispatch => {
   return response;
 };
 
+export const signup = (user) => async dispatch => {
+  const { username, email, password, policyIds } = user;
+  const response = await csrfFetch('/api/users', {
+    method: "POST",
+    body: JSON.stringify({
+      username,
+      email,
+      password,
+      policyIds
+    }),
+  });
+  const data = await response.json();
+  dispatch(setUser(data.user));
+  return response;
+}
 export default sessionReducer;
