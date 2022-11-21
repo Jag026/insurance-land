3067cb3b23fb1a35f870e2ee99501bfb997d75aa
Drew <DGriffin@appacademy.io>
2022-11-21 16:35:37 -0600

    Updated ux

--- frontend/src/components/CompanyMarketplace/index.js
+++ frontend/src/components/CompanyMarketplace/index.js
@@ -18,8 +18,8 @@ function CompanyMarketplace() {
                 <div className="policy-title-container">
                   <h2 className="policy-name">{policy.name}</h2>
                 </div>
-               <p>Description: <span className="inner-text">${policy.description}</span></p>
-               <p>Issuer: <span className="inner-text">${policy.companyName}</span></p>
+               <p>Description: <span className="inner-text">{policy.description}</span></p>
+               <p>Issuer: <span className="inner-text">{policy.companyName}</span></p>
                <p>Monthly Premium: <span className="inner-text">${policy.premium}</span></p>
                <li><span onClick={goUserLogin} className="login">Login</span> as user to add policy</li>
               </div>
